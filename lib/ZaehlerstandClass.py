5880a278cca6aa9d86f18681f1f7778ea2b7bf24
jomjol <30766535+jomjol@users.noreply.github.com>
2019-09-06 22:34:02 +0200

    Update to V2.1.0
    Integration of direct request to Image provider

--- lib/ZaehlerstandClass.py
+++ lib/ZaehlerstandClass.py
@@ -2,6 +2,7 @@ import configparser
 import lib.ReadAnalogNeedleClass
 import lib.CutImageClass
 import lib.ReadDigitalDigitClass
+import lib.LoadFileFromHTTPClass
 import math
 
 class Zaehlerstand:
@@ -15,60 +16,58 @@ class Zaehlerstand:
         self.readDigitalDigit = lib.ReadDigitalDigitClass.ReadDigitalDigit()
         print('Digital Model Init Done')
         self.CutImage = lib.CutImageClass.CutImage()
-
-#        if config.has_option('Analog_Counter', 'LogImageLocation'):
-#            self.log_Image = config['Analog_Counter']['LogImageLocation']
-#            if config.has_option('Analog_Counter', 'LogNames'):
-#                zw_LogNames = config.get('Analog_Counter', 'LogNames').split(',')
-#                self.LogNames = []
-#                for nm in zw_LogNames:
-#                      self.LogNames.append(nm.strip())
-#            else:
-#                self.LogNames = ''
-#        else:
-#            self.log_Image = ''
+        print('Digital Model Init Done')
+        self.LoadFileFromHTTP = lib.LoadFileFromHTTPClass.LoadFileFromHttp()
 
 
-    def getZaehlerstand(self, img_file, simple = True):
-        print('Start CutImage')
-        resultcut = self.CutImage.Cut(img_file)
-        print('Start DigitalDigit Readout')
-        resultdigital = self.readDigitalDigit.Readout(resultcut[1])
-        print('Start AnalogNeedle Readout')
-        resultanalog = self.readAnalogNeedle.Readout(resultcut[0])
-        
-        vorkomma = self.DigitalReadoutToValue(resultdigital)
-        nachkomma = self.AnalogReadoutToValue(resultanalog)
-        zaehlerstand = str(vorkomma.lstrip("0")) + '.' + str(nachkomma)
+    def getZaehlerstand(self, url, simple = True, preValue = ''):
+        txt, logtime = self.LoadFileFromHTTP.LoadImageFromURL(url, './image_tmp/original.jpg')
 
-        print('Start Making Zaehlerstand')
+        if len(txt) == 0:
+            print('Start CutImage')
+            resultcut = self.CutImage.Cut('./image_tmp/original.jpg')
 
-        txt = zaehlerstand + '\t' + vorkomma  + '\t' + nachkomma 
+            print('Start AnalogNeedle Readout')
+            resultanalog = self.readAnalogNeedle.Readout(resultcut[0], logtime)
 
-        if not simple:
-            txt = txt + '<p>Aligned Image: <p><img src=/image_tmp/alg.jpg></img><p>'
-            txt = txt + 'Digital Counter: <p>'
-            for i in range(len(resultdigital)):
-                if resultdigital[i] == 'NaN':
-                    zw = 'NaN'
-                else:
-                    zw = str(int(resultdigital[i]))
-                txt += '<img src=/image_tmp/'+  str(resultcut[1][i][0]) + '.jpg></img>' + zw
-            txt = txt + '<p>'
-            txt = txt + 'Analog Meter: <p>'
-            for i in range(len(resultanalog)):
-                txt += '<img src=/image_tmp/'+  str(resultcut[0][i][0]) + '.jpg></img>' + "{:.1f}".format(resultanalog[i])
-            txt = txt + '<p>'
-        print('Get Zaehlerstand done')
+            print('Start DigitalDigit Readout')
+            resultdigital = self.readDigitalDigit.Readout(resultcut[1], logtime)
+            
+            nachkomma = self.AnalogReadoutToValue(resultanalog)
+            vorkomma = self.DigitalReadoutToValue(resultdigital)
+
+            self.LoadFileFromHTTP.PostProcessLogImageProcedure(True)
+
+            zaehlerstand = str(vorkomma.lstrip("0")) + '.' + str(nachkomma)
+
+            print('Start Making Zaehlerstand')
+
+            txt = zaehlerstand + '\t' + vorkomma  + '\t' + nachkomma 
+
+            if not simple:
+                txt = txt + '<p>Aligned Image: <p><img src=/image_tmp/alg.jpg></img><p>'
+                txt = txt + 'Digital Counter: <p>'
+                for i in range(len(resultdigital)):
+                    if resultdigital[i] == 'NaN':
+                        zw = 'NaN'
+                    else:
+                        zw = str(int(resultdigital[i]))
+                    txt += '<img src=/image_tmp/'+  str(resultcut[1][i][0]) + '.jpg></img>' + zw
+                txt = txt + '<p>'
+                txt = txt + 'Analog Meter: <p>'
+                for i in range(len(resultanalog)):
+                    txt += '<img src=/image_tmp/'+  str(resultcut[0][i][0]) + '.jpg></img>' + "{:.1f}".format(resultanalog[i])
+                txt = txt + '<p>'
+            print('Get Zaehlerstand done')
         return txt
 
     def AnalogReadoutToValue(self, res_analog):
         prev = -1
         erg = ''
-#        for item in res_analog[::-1]:
-        for item in res_analog:
+        for item in res_analog[::-1]:
+#        for item in res_analog:
             prev = self.ZeigerEval(item, prev)
-            erg = erg + str(int(prev))
+            erg = str(int(prev)) + erg
         return erg
 
     def ZeigerEval(self, zahl, ziffer_vorgaenger):
